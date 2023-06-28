ec0022e3e055d6bf22d3c8344f52a9ae809241a3
Wells <wellsling68@gmail.com>
2023-06-28 12:27:37 -0700

    Day 5

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 let textInput = document.querySelector("#text-input");
 let log = document.querySelector("#consoleLog");
 log.addEventListener("click", function(){
-    textInput.value = 20;
     console.log(textInput.value);
-    sum = textInput.value;
 })
 
 let hide = document.querySelector("#hide");
@@ -17,4 +15,35 @@ show.addEventListener("click", function(){
     hideable.style.display = "block";
 })
 
+let input = document.querySelector("textarea");
+let checkbox = document.querySelector("#checkbox");
+let consolelog = document.querySelector("#checked");
+consolelog.addEventListener("click", function(){
+    if(checkbox.checked == true)
+    {
 
+        console.log(input.value);
+    }
+})
+
+
+
+let car = { brand: "honda", model: "civic", year: "2024", fuel: "gas"};
+
+for(prop in car)
+{
+    console.log(prop);
+    console.log(car[prop]);
+}
+
+
+let fetch_button = document.querySelector("#fetch");
+fetch_button.addEventListener("click", function() {
+    fetch('https://reqres.in/api/users')
+    .then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+    })
+
+});
