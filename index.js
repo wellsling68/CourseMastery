a7ecf2dbce58eafc955a8ba530cf129a4cbf95fc
Wells <wellsling68@gmail.com>
2023-07-06 21:23:17 -0700

    Changed Stuff

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
 //input
-let textInput = document.querySelector("#text-input");
-let log = document.querySelector("#consoleLog");
-log.addEventListener("click", function(){
-    console.log(textInput.value);
-})
+let userInput = document.querySelector("#userInput");
+let inputStorage = [];
+let count = 0;
+let pageno = 0;
 
-let hide = document.querySelector("#hide");
-hide.addEventListener("click", function(){
-    hideable.style.display = "none";
-})
-
-let show = document.querySelector("#show");
-show.addEventListener("click", function(){
-    hideable.style.display = "block";
-})
-
-let input = document.querySelector("textarea");
-let checkbox = document.querySelector("#checkbox");
-let consolelog = document.querySelector("#checked");
-consolelog.addEventListener("click", function(){
-    if(checkbox.checked == true)
-    {
-
-        console.log(input.value);
+userInput.addEventListener("keyup", function(e){
+    if(e.keyCode === 13){
+        console.log(userInput.value);
+        fetch("https://api.openai.com/v1/completions", {
+            method: 'POST',
+            body: JSON.stringify({
+                "model":"text-davinci-003",
+                "prompt": `Please provide a short description of the course ` + userInput.value + `, including a short description of the course and topics covered. Each topic should be formatted with a dash at the beginning and placed on a separate line.`,
+                "temperature": 0,
+                "max_tokens": 20,
+    
+            }),
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: "Bearer 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"
+            }
+        })
+        .then(response => {
+            return response.json();
+        }).then(data =>{
+            output.innerHTML = data.choices[0].text.replaceAll("\n", "<br>");
+            inputStorage[count] =  data.choices[0].text.replaceAll("\n", "<br>");
+            continueButton.style.display = "block";
+            previousButton.style.display = "block";
+            nextButton.style.display = "block";
+        })
+        
     }
-})
+    
+});
 
-let post_button = document.querySelector("#post");
-post_button.addEventListener("click", function(){
+let continueButton = document.querySelector("#continue");
+continueButton.addEventListener("click", function(){
+    count++;
     fetch("https://api.openai.com/v1/completions", {
         method: 'POST',
         body: JSON.stringify({
             "model":"text-davinci-003",
-            "prompt": "Say hello to me.",
-            "temperature": 0.2,
+            "prompt": "Continue explaining topic " + count + "in " + userInput.value + ". Make sure to go into detail on each of the topics and provide necessary supplemental information such as formulas.",
+            "temperature": 0,
             "max_tokens": 20,
 
         }),
         headers: {
             'Content-type': 'application/json',
-            Authorization: "Bearer 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"
+            Authorization: "Bearer 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"
         }
     })
     .then(response => {
         return response.json();
     }).then(data =>{
-        console.log(data.choices[0].text.replaceAll("\n", ""));
+        output.innerHTML = data.choices[0].text.replaceAll("\n", "<br>");
+        console.log(count);
+        inputStorage[count] =  data.choices[0].text.replaceAll("\n", "<br>");
+        pageno=count;
     })
     
+})
+
+let previousButton = document.querySelector("#previous");
+previousButton.addEventListener("click", function(){
+    if(pageno-1>=0){
+        pageno--;
+        output.innerHTML = inputStorage[pageno];
+    }
+})
+let nextButton = document.querySelector("#next");
+nextButton.addEventListener("click", function(){
+    if(pageno+1<=count){
+        pageno++;
+        output.innerHTML = inputStorage[pageno];
+    }
 })
